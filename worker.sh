198957a75d14ed68f92ffbb7f91ac5aa45fac4c0
sami rhimi <13876753+samirhimi@users.noreply.github.com>
2025-01-12 10:15:20 +0100

    Update worker.sh

--- worker.sh
+++ worker.sh
@@ -3,7 +3,7 @@ echo "bootstrapping workers nodes"
 
 # Setting and configuring the VM
 apt-get update && apt-get upgrade -y && apt-get autoremove -y
-apt install -y apt-transport-https curl 
+apt-get install -y apt-transport-https curl 
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 systemctl restart sshd
 PASS=$(echo "k8s" | openssl passwd -1 -stdin)
@@ -12,7 +12,7 @@ echo "student  ALL=(ALL:ALL) NOPASSWD: ALL"  >> /etc/sudoers.d/student
 
 # Add host mapping
 cat <<EOF > /etc/hosts
-192.168.2.200 cp.lab.local  cp 
-192.168.2.201 worker1.lab.local  worker1
-192.168.2.202 worker2.lab.local  worker2
+172.18.5.10 cp.lab.local  cp
+172.18.5.11 worker1.lab.local  worker1
+172.18.5.12 worker2.lab.local  worker2
 EOF
